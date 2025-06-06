84d6fb724bea175049d40ed9c8f38d199e9aa8a0
Konstantin Lyashenko <lyashenkokv@gmail.com>
2025-06-06 19:09:28 +0300

    Initial Commit

---
+++ SpendWise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpendWise
+//
+//  Created by Konstantin Lyashenko on 06.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpendWise/SpendWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendWiseApp.swift
+//  SpendWise
+//
+//  Created by Konstantin Lyashenko on 06.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct SpendWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

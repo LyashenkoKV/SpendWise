e2f39be8ff044b6c18bbbee35712d5f2b9171222
Konstantin Lyashenko <lyashenkokv@gmail.com>
2025-06-07 11:11:55 +0300

    feat (common): Добавить логгер

---
+++ Modules/Common/GlobalConstants.swift
@@ -0,0 +1,16 @@
+//
+//  GlobalConstants.swift
+//  Common
+//
+//  Created by Konstantin Lyashenko on 07.06.2025.
+//
+
+import Foundation
+
+public enum GlobalConstants: String {
+
+    // Logger
+
+    case logSubsystem = "KV.Lyashenko.SpendWise"
+    case logCategory = "account"
+}
